990a7be3761143de69786923cf54ab30710010ba
ML-dot77 <lyonsmf@yahoo.com>
2025-10-16 18:09:36 -0400

    Add React Router routes for Home and Admin

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
 import Admin from "./Admin";
+
+function Home() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Welcome to Rowvember</h1>
+      <p>Visit the <a href="/admin">Admin Dashboard</a> to manage entries.</p>
+    </div>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Home />} />
         <Route path="/admin" element={<Admin />} />
       </Routes>
     </BrowserRouter>
