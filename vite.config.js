e4d0637df0406cc65e6f7147a98da8f7797b9706
ML-dot77 <lyonsmf@yahoo.com>
2025-10-18 14:10:36 -0400

    Add alias for Recharts to fix Vercel build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      recharts: "recharts/es6/index.js",
+    },
+  },
   optimizeDeps: {
     include: ["recharts"],
   },
